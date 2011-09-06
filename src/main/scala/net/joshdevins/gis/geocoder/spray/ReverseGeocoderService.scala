92077fe4bd68a2b06f0312963c7781e8d94596b2
Josh Devins <info@joshdevins.net>
2011-09-06 10:23:45 +0200

    Switched to scala version of indexer

--- src/main/scala/net/joshdevins/gis/geocoder/spray/ReverseGeocoderService.scala
+++ src/main/scala/net/joshdevins/gis/geocoder/spray/ReverseGeocoderService.scala
@@ -1,29 +1,23 @@
 package net.joshdevins.gis.geocoder.spray
 
-import scala.collection.JavaConversions._
-
 import cc.spray._
 import cc.spray.directives.DoubleNumber
 import cc.spray.marshalling.SprayJsonMarshalling._
 import cc.spray.json._
 import cc.spray.json.DefaultJsonProtocol._
-
-import net.joshdevins.gis.geocoder.ShapefileIndexer
+import net.joshdevins.gis.geocoder.ShapefileIndex
 
 trait ReverseGeocoderService extends Directives {
 
-  val neighbourhoods = new ShapefileIndexer("data/build/flickr-shapes/neighbourhoods.shp")
+  val neighbourhoods = new ShapefileIndex("neighbourhoods", "data/build/flickr-shapes/neighbourhoods.shp")
 
   val reverseGeocoderService = {
 
     path(DoubleNumber ~ "," ~ DoubleNumber) { (lat, lon) =>
       get {
 
-        val woeidJavaSet = neighbourhoods.getWOEIDSetForCoordinates(lat, lon)
-        val woeidListJavaLong = asScalaSet(woeidJavaSet).toList
-        val woeidList = woeidListJavaLong.map(x => x.longValue())
-
-        _.complete(woeidList.toJson)
+        val woeidSet = neighbourhoods.getWOEIDSetForCoordinates(lat, lon)
+        _.complete(woeidSet.toList.toJson)
       }
     }
   }
